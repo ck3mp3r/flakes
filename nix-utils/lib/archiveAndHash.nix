a55c1bf5973fe4f68713367a95c135a4430286f4
Christian Kemper <christian.kemper@me.com>
2025-08-02 19:02:06 +0100

    refactor archiveAndHash to return enriched attribute set
    - Change output from attribute set with three values to merged artifact set including nixHash, sha256, pname, version, and meta
    - Add pname, version, and meta fields, defaulting to name, unknown, and empty set if not present in drv
    - Use artifact as base for returned set, merging additional attributes
    - Improve formatting and consistency in let bindings and function arguments

--- nix-utils/lib/archiveAndHash.nix
+++ nix-utils/lib/archiveAndHash.nix
@@ -1,24 +1,28 @@
-{
-  pkgs,
+{ pkgs,
   drv,
   name ? "artifact",
-  compress ? true,
-}: let
-  artifact =
-    if compress
-    then
-      pkgs.runCommand "${name}.tgz" {buildInputs = [pkgs.gnutar pkgs.gzip];} ''
-        tar czf $out -C ${drv} .
-      ''
-    else drv;
+  compress ? true
+}:
 
-  nixHash = pkgs.runCommand "${name}-nix.sha256" {buildInputs = [pkgs.nix];} ''
+let
+  artifact = if compress then
+    pkgs.runCommand "${name}.tgz" { buildInputs = [ pkgs.gnutar pkgs.gzip ]; } ''
+      tar czf $out -C ${drv} .
+    ''
+  else
+    drv;
+
+  nixHash = pkgs.runCommand "${name}-nix.sha256" { buildInputs = [ pkgs.nix ]; } ''
     nix-hash --type sha256 --flat --base32 ${artifact} > $out
   '';
 
-  sha256 = pkgs.runCommand "${name}.sha256" {buildInputs = [pkgs.coreutils];} ''
+  sha256 = pkgs.runCommand "${name}.sha256" { buildInputs = [ pkgs.coreutils ]; } ''
     sha256sum ${artifact} | cut -f1 -d' ' > $out
   '';
-in {
-  inherit artifact nixHash sha256;
-}
+in
+  artifact // {
+    inherit nixHash sha256;
+    pname = drv.pname or name;
+    version = drv.version or "unknown";
+    meta = drv.meta or {};
+  }
