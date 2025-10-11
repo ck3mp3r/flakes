3c27ddf74b556374f22fccc94d99345fe89404ae
Christian Kemper <christian.kemper@me.com>
2025-10-11 11:27:10 +0100

    fix: use fixDarwinDylibNames hook correctly
    Adds fixDarwinDylibNames to nativeBuildInputs instead of manual postInstall.
    The hook automatically fixes dylib install names during the fixup phase.

--- rustix/lib/rust/build.nix
+++ rustix/lib/rust/build.nix
@@ -23,10 +23,6 @@
         buildInputs = extraArgs.buildInputs or [];
         nativeBuildInputs = extraArgs.nativeBuildInputs or [] 
           ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [pkgs.fixDarwinDylibNames];
-        
-        postInstall = pkgs.lib.optionalString pkgs.stdenv.isDarwin ''
-          ${pkgs.fixDarwinDylibNames}/bin/fixDarwinDylibNames $out/bin/*
-        '';
       }
       // extraArgs);
 in
